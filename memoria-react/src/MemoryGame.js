a90ed49ddabff29a8ca267b0545a1fdfc6d5eea0
rayaneanjos <raydosanjos98@gmail.com>
2023-03-20 14:50:25 -0300

    Criando o tabuleiro

--- memoria-react/src/MemoryGame.js
+++ memoria-react/src/MemoryGame.js
@@ -1,11 +1,17 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import GameBoard from './components/GameBoard'
 import GameOver from './components/GameOver'
+import game from './game/game'
 
 
 
 export default function MemoryGame(){
-
+    const [cards, setCards] = useState([])
     const [gameOver, setGameOver] = useState(false)
+
+    useEffect(()=>{
+       setCards(game.createCardsFromTechs())
+    },[])
 
     function restart(){
         setGameOver(false)
@@ -13,6 +19,7 @@ export default function MemoryGame(){
 
     return(
         <div>
+            <GameBoard cards={cards}></GameBoard>
             <GameOver show={gameOver} handleRestart={restart}></GameOver>
         </div>
     )
--- memoria-react/src/components/CardElement.js
+++ memoria-react/src/components/CardElement.js
@@ -5,7 +5,9 @@ export default function CardElement(props){
     return(
         <div id={props.card.id} className="card">
             <div className='card_front'>
-                <img className='icon' src={`assets/images/${props.card.icon}.png`} alt={props.card.icon}></img>
+                <img className='icon' 
+                src={`assets/images/${props.card.icon}.png`}
+                alt={props.card.icon}></img>
             </div>
             <div className='card_back'>
                 {"</>"}
