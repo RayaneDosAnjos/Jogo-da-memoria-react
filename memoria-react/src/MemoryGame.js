94d1283d4a3ef9f5bb6931536373d8f851057af1
rayaneanjos <raydosanjos98@gmail.com>
2023-03-20 15:32:17 -0300

    Juntando as peças

--- memoria-react/src/MemoryGame.js
+++ memoria-react/src/MemoryGame.js
@@ -14,12 +14,34 @@ export default function MemoryGame(){
     },[])
 
     function restart(){
+        game.clearCards()
+        setCards(game.createCardsFromTechs())
         setGameOver(false)
+    }
+    function handleFlip(card){
+        if(game.setCard(card.id)){
+            if(game.secondCard){
+                if (game.checkMatch()){
+                    game.clearCards()
+                    if (game.checkGameOver()) {
+                        //game over
+                        setGameOver(true)
+                    }
+                }else{
+                    setTimeout(()=>{
+                        //no match
+                            game.unflipCards()
+                            setCards([...game.cards])
+                    },1000)
+                }
+            }
+            }
+            setCards([...game.cards])
     }
 
     return(
         <div>
-            <GameBoard cards={cards}></GameBoard>
+            <GameBoard handleFlip={handleFlip} cards={cards}></GameBoard>
             <GameOver show={gameOver} handleRestart={restart}></GameOver>
         </div>
     )
